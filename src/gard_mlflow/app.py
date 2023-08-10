ee16b694db13e6df392916ff1d14d69dca08609a
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-08-10 14:11:24 -0400

    Restrict user creation to admins

--- src/gard_mlflow/app.py
+++ src/gard_mlflow/app.py
@@ -3,6 +3,11 @@ from mlflow.server import app
 import mlflow.server.auth
 
 def create_app(app: Flask = app):
+    # Restrict user creation to admins only
     mlflow.server.auth.UNPROTECTED_ROUTES = []
+    mlflow.server.auth.BEFORE_REQUEST_VALIDATORS.update({
+        (mlflow.server.auth.SIGNUP, "GET"): mlflow.server.auth.validate_can_update_user_admin,
+        (mlflow.server.auth.CREATE_USER, "POST"): mlflow.server.auth.validate_can_update_user_admin,
+    })
     app = mlflow.server.auth.create_app(app)
     return app
